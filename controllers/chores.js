846cf5bd7b682e3758bb2257ef2ef752aac047a8
Hein Swan Htet <hshscientist@gmail.com>
2024-03-12 16:59:53 +0630

    Added functionalities to all CRUD functions

--- controllers/chores.js
+++ controllers/chores.js
@@ -1,26 +1,54 @@
 const Chore = require('../models/Chore')
 const asyncWrapper = require('../middleware/async')
+const { createCustomError } = require('../errors/custom-errors')
 
-const getAllChores = (req, res) => {
-    res.send('get all chores')
-}
+const getAllChores = asyncWrapper(async (req, res) => {
+    const chores = await Chore.find({})
+    res.status(200).json({ chores })
+})
 
-const createChore = asyncWrapper(async (req, res) => {
+const createChore = asyncWrapper(async (req, res, next) => {
+    const getChore = await Chore.findOne({ title: req.body.title })
+    if (getChore) {
+        return next(createCustomError(`title: <${req.body.title}> already exists`, 400))
+    }
     const chore = await Chore.create(req.body)
     res.status(201).json({ chore })
 })
 
-const getSingleChore = (req, res) => {
-    res.send(`get a single chore: ${req.params.id}`)
-}
+const getSingleChore = asyncWrapper(async (req, res, next) => {
+    const { id: choreID } = req.params
+    const chore = await Chore.findOne({ _id: choreID })
+    if (!chore) {
+        return next(createCustomError(`Not found with chore id: ${choreID}`, 404))
+    }
+    res.status(200).json({ chore })
+})
 
-const updateChore = (req, res) => {
-    res.send(`update a chore: ${req.body.name}, ${req.params.id}`)
-}
+const updateChore = asyncWrapper(async (req, res, next) => {
+    const { id: choreID } = req.params
+    const chore = await Chore.findOneAndUpdate(
+        { _id: choreID },
+        req.body,
+        {
+            new: true,
+            runValidators: true
+        }
+    )
+    if (!chore) {
+        return next(createCustomError(`Not found with chore id: ${choreID}`, 404))
+    }
+    res.status(200).json({ chore })
+})
 
-const deleteChore = (req, res) => {
-    res.send(`delete a chore: ${req.params.id}`)
-}
+const deleteChore = asyncWrapper(async (req, res, next) => {
+    const { id: choreID } = req.params
+    const chore = await Chore.findOneAndDelete({ _id: choreID })
+    if (!chore) {
+        return next(createCustomError(`Not found with chore id: ${choreID}`, 404))
+    }
+    res.status(200).json({ chore })
+})
 
 module.exports = {
     getAllChores,
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -4,7 +4,8 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     if (err instanceof CustomAPIError) {
         return res.status(err.statusCode).json({ msg: err.message })
     }
-    return res.status(500).json({ msg: 'Oops! Something went wrong. Please try again later' })
+    return res.status(500).json({ message: err })
+    // return res.status(500).json({ msg: 'Oops! Something went wrong. Please try again later' })
 }
 
 module.exports = errorHandlerMiddleware
